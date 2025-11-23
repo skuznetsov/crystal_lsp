d28750d507120c1c430c858d57da83f89f01a261
Sergey Kuznetsov <sergey@iterudit.com>
2025-11-23 10:53:37 -0500

    Add heredoc error coverage (pending)

---
+++ spec/parser/parser_heredoc_errors_spec.cr
@@ -0,0 +1,51 @@
+require "spec"
+
+require "../../src/compiler/frontend/parser"
+
+describe "CrystalV2::Compiler::Frontend::Parser" do
+  describe "heredoc error handling" do
+    pending "heredoc indent diagnostics not implemented yet" do
+      it "reports indent error" do
+        source = "<<-HERE\n One\n  #{1}\n  HERE"
+        parser = CrystalV2::Compiler::Frontend::Parser.new(
+          CrystalV2::Compiler::Frontend::Lexer.new(source)
+        )
+        parser.parse_program
+
+        parser.diagnostics.should_not be_empty
+      end
+    end
+
+    pending "heredoc interpolation diagnostics not implemented yet" do
+      it "reports heredoc inside interpolation" do
+        source = %(\#{<<-HERE}\nHERE)
+        parser = CrystalV2::Compiler::Frontend::Parser.new(
+          CrystalV2::Compiler::Frontend::Lexer.new(source)
+        )
+        parser.parse_program
+
+        parser.diagnostics.should_not be_empty
+      end
+    end
+
+    it "reports missing terminator" do
+      source = "<<-FOO\n1\nFOO.bar"
+      parser = CrystalV2::Compiler::Frontend::Parser.new(
+        CrystalV2::Compiler::Frontend::Lexer.new(source)
+      )
+      parser.parse_program
+
+      parser.diagnostics.should_not be_empty
+    end
+
+    it "reports unexpected EOF" do
+      source = "<<-HEREDOC"
+      parser = CrystalV2::Compiler::Frontend::Parser.new(
+        CrystalV2::Compiler::Frontend::Lexer.new(source)
+      )
+      parser.parse_program
+
+      parser.diagnostics.should_not be_empty
+    end
+  end
+end
