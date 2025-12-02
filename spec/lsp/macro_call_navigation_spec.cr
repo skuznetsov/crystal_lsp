4fb1030032e2577e7e4896e83acb80b6f5cf2b4c
Sergey Kuznetsov <sergey@iterudit.com>
2025-12-02 13:15:43 -0500

    feat: resolve macro calls to macro definitions

---
+++ spec/lsp/macro_call_navigation_spec.cr
@@ -0,0 +1,27 @@
+require "spec"
+
+require "./support/server_helper"
+
+describe CrystalV2::Compiler::LSP::Server do
+  it "navigates from macro call to macro definition" do
+    source = <<-CR
+    macro hello(name)
+      def hello_{{name.id}}
+        42
+      end
+    end
+
+    hello world
+    CR
+
+    server = CrystalV2::Compiler::LSP::Server.new(IO::Memory.new, IO::Memory.new, CrystalV2::Compiler::LSP::ServerConfig.new(background_indexing: false, project_cache: false))
+    uri = server.spec_store_document(source, nil, "/tmp/macro_call.cr")
+
+    line_idx = source.lines.index { |l| l.includes?("hello world") }.not_nil!
+    char_idx = source.lines[line_idx].index("hello").not_nil!
+
+    definition = server.spec_definition(uri, line_idx, char_idx)
+    definition["result"].should_not be_nil
+    definition["result"].as_a.size.should be >= 1
+  end
+end
