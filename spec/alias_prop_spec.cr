8270e45f097e0e691e9a7167ceadf2069c63a651
Sergey Kuznetsov <sergey@iterudit.com>
2025-12-10 22:39:58 -0500

    fix(test): convert alias operands spec to top-level

--- spec/alias_prop_spec.cr
+++ spec/alias_prop_spec.cr
@@ -1,16 +1,14 @@
 require "spec"
 require "../src/compiler/mir/mir"
 
-module AliasPropSpec
-  describe "operands collection" do
-    it "includes operands for RC ops" do
-      func = Crystal::MIR::Function.new(0_u32, "test", Crystal::MIR::TypeRef::VOID)
-      builder = Crystal::MIR::Builder.new(func)
-      ptr = builder.const_nil
-      inc = Crystal::MIR::RCIncrement.new(func.next_value_id, ptr, false)
-      dec = Crystal::MIR::RCDecrement.new(func.next_value_id, ptr, false)
-      expect(inc.operands).to eq [ptr]
-      expect(dec.operands).to eq [ptr]
-    end
+describe "RC operands" do
+  it "includes ptr operand for RC ops" do
+    func = Crystal::MIR::Function.new(0_u32, "test", Crystal::MIR::TypeRef::VOID)
+    builder = Crystal::MIR::Builder.new(func)
+    ptr = builder.const_nil
+    inc = Crystal::MIR::RCIncrement.new(func.next_value_id, ptr, false)
+    dec = Crystal::MIR::RCDecrement.new(func.next_value_id, ptr, false)
+    inc.operands.should eq [ptr]
+    dec.operands.should eq [ptr]
   end
 end
