19b798ac703e9395e72b1d19d84c23bff3edb109
Sergey Kuznetsov <sergey@iterudit.com>
2025-12-10 23:19:20 -0500

    test(llvm): add struct mapping coverage

---
+++ spec/llvm_type_mapper_struct_spec.cr
@@ -0,0 +1,14 @@
+require "./spec_helper"
+
+describe Crystal::MIR::LLVMTypeMapper do
+  it "maps struct with fields to named type" do
+    registry = Crystal::MIR::TypeRegistry.new
+    t = registry.create_type(Crystal::MIR::TypeKind::Struct, "Foo", 16, 8)
+    t.add_field("a", Crystal::MIR::TypeRef::INT32, 0)
+    t.add_field("b", Crystal::MIR::TypeRef::INT64, 8)
+
+    mapper = Crystal::MIR::LLVMTypeMapper.new(registry)
+    mapper.llvm_type(t).should eq("%Foo")
+    mapper.llvm_alloca_type(t).should eq("%Foo")
+  end
+end
